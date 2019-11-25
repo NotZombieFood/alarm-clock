a36e9b45dfcde05073f91e27d84352262b7e935c
Gerardo Cruz <gerardocd1000@gmail.com>
2019-11-24 21:31:54 -0600

    Added design templates and some js
    I added the splash screen and basic css, cant test it since this laptop doesnt have everything needed

--- app/static/main.js
+++ app/static/main.js
@@ -10,3 +10,20 @@ function formFail(error_message) {
 }
 
 
+// show a splash screen when entering first time :)
+$.sessionStorage.settings = {
+    cookiePrefix : 'html5fallback:sessionStorage:', / Prefix for the Session Storage substitution cookies
+    cookieOptions : {
+        path : '/', // Path for the cookie
+        domain : document.domain, // Domain for the cookie
+        expires: undefined // Days left for cookie expiring (by default expires with the session)
+    }
+};
+//This will be checked every session
+
+var isNew = $.sessionStorage.getItem('new');
+console.log(isNew);
+if (isNew != "Old session"){
+  $("#splash").fadeIn();
+  $.sessionStorage.setItem('new', "Old session");
+}
